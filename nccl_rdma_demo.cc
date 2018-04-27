650c14cf29901398c50353c5306eee30e0fa8ac1
typhoonzero <typhoonzero1986@gmail.com>
2018-04-27 16:29:40 +0800

    update

--- nccl_rdma_demo.cc
+++ nccl_rdma_demo.cc
@@ -1,33 +1,106 @@
+#include <sstream>
+#include <cassert>
+
 #include "nccl.h"
 #include "redis_client.h"
 
-void ncclCommInitAllNodes(ncclComm_t* comm,
-                                  int ndev,
-                                  const int* devlist,
-                                  const ncclUniqueId& id) {
-  ncclGroupStart();
-  for (int i=0; i<ndev; i++) {
-    cudaSetDevice(devlist[i]);
-    ncclCommInitRank(comm+i, ndev, id, i);
+#define CUDACHECK(cmd) do {                         \
+  cudaError_t e = cmd;                              \
+  if( e != cudaSuccess ) {                          \
+    printf("Failed: Cuda error %s:%d '%s'\n",             \
+        __FILE__,__LINE__,cudaGetErrorString(e));   \
+    exit(EXIT_FAILURE);                             \
+  }                                                 \
+} while(0)
+
+#define NCCLCHECK(cmd) do {                         \
+  ncclResult_t r = cmd;                             \
+  if (r!= ncclSuccess) {                            \
+    printf("Failed, NCCL error %s:%d '%s'\n",             \
+        __FILE__,__LINE__,ncclGetErrorString(r));   \
+    exit(EXIT_FAILURE);                             \
+  }                                                 \
+} while(0)
+
+
+/*
+ * This is an example of using nccl to do multi node multi GPU communication
+ * without using MPI, in additional to the original example at:
+ * https://docs.nvidia.com/deeplearning/sdk/nccl-developer-guide/index.html#multidevprothrd
+*/
+
+std::vector<std::string> split(const std::string &s, char delim) {
+  std::stringstream ss(s);
+  std::string item;
+  std::vector<std::string> tokens;
+  while (getline(ss, item, delim)) {
+      tokens.push_back(item);
   }
-  ncclGroupEnd();
+  return tokens;
+}
+
+void InitMemory(int gpu_count, int node_id, int size,
+                float** sendbuff, float** recvbuff,
+                cudaStream_t* s) {
+  for (int i = 0; i < gpu_count; ++i) {
+    CUDACHECK(cudaSetDevice(node_id * gpu_count + i));
+    CUDACHECK(cudaMalloc(sendbuff + i, size * sizeof(float)));
+    CUDACHECK(cudaMalloc(recvbuff + i, size * sizeof(float)));
+    CUDACHECK(cudaMemset(sendbuff[i], 1, size * sizeof(float)));
+    CUDACHECK(cudaMemset(recvbuff[i], 0, size * sizeof(float)));
+    CUDACHECK(cudaStreamCreate(s+i));
+  }
 }
 
 int main(int argc, char** argv) {
-  if (argc < 1) {
-    std::cerr << "must provide cluster total trainer count" << std::endl;
+  if (argc != 4) {
+    std::cout << "Usage: demo [redisip:port] [node count] [node id]" << std::endl;
+    exit(1);
   }
-  int node_count = atoi([argv[0]]);
-  NcclIdServer id_server("127.0.0.1", 6379);
+  std::string endpoint(argv[1]);
+  auto ep_parts = split(endpoint, ':');
+  std::cout << "parts: " << ep_parts.size()
+            << " endpoints: " << endpoint << std::endl;
+
+  assert(ep_parts.size() == 2);
+
+  int node_count = atoi(argv[2]);
+  int node_id = atoi(argv[3]);
+
+  NcclIdServer id_server(ep_parts[0], atoi(ep_parts[1].c_str()));
 
   ncclUniqueId id;
-  ncclGetUniqueId(&id);
+  if (node_id == 0) {
+    ncclGetUniqueId(&id);
+    id_server.SetId(id);
+  }
+  auto nccl_id = id_server.GetClusterId(node_count);
 
-  id_server.SetIds(0, id);
-  auto all_ids = id_server.GetClusterIds(node_count);
-  ncclComm_t comm[node_count * 4];  // 4 GPU
+  int gpu_count = 4;
+  ncclComm_t comms[gpu_count];  // 4 GPU
   int devlist[4] = {0,1,2,3};
-  for ()
-    ncclCommInitAllNodes(comm, 4, devlist, );
+  float** sendbuff = (float**)malloc(nDev * sizeof(float*));
+  float** recvbuff = (float**)malloc(nDev * sizeof(float*));
+  cudaStream_t* s = (cudaStream_t*)malloc(sizeof(cudaStream_t)*gpu_count);
+
+  // alloc GPU memory and set sendbuff to value 1.
+  InitMemory(gpu_count, node_id, 4096, sendbuff, recvbuff, s);
+
+  for (int i = 0; i < gpu_count; ++i) {
+    std::cout << "initializing rank: " << node_id * 4 + i << std::endl;
+    cudaSetDevice(node_id * gpu_count + i);
+    ncclCommInitRank(comms, node_count * gpu_count, nccl_id, node_id * gpu_count + i);
+  }
+
+  // do allreduce
+   NCCLCHECK(ncclGroupStart());
+  for (int i = 0; i < gpu_count; i++)
+     NCCLCHECK(ncclAllReduce((const void*)sendbuff[i], (void*)recvbuff[i],
+           4096, ncclFloat, ncclSum,
+           comms[i], s[i]));
+  NCCLCHECK(ncclGroupEnd());
+
+  for (int i = 0; i < gpu_count; i++) {
+    ncclCommDestroy(comms[i]);
   }
 }
--- redis_client.h
+++ redis_client.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <iostream>
 #include <unistd.h>
+#include <string.h>
 
 #include "third_party/hiredis/hiredis.h"
 #include "nccl.h"
@@ -25,64 +26,66 @@ class NcclIdServer {
 
   ~NcclIdServer() { redisFree(ctx_); }
 
-  bool SetIds(int trainer_id, const ncclUniqueId& uniq_id) {
-    char commands[3][NCCL_UNIQUE_ID_BYTES];
-    size_t lens[3];
+  bool SetId(const ncclUniqueId& uniq_id) {
+    std::vector<const char*> command;
+    std::vector<size_t> lens;
     bool ret = false;
-    for (auto &per_id : ids) {
-      command[0] = "SET";
-      lens[0] = strlen(command[0]);
-      sprintf(command[1], "trainer:%03d", trainer_id);
-      lens[1] = strlen(command[1]);
-      memcpy(command[2], uniq_id.internal, NCCL_UNIQUE_ID_BYTES);
-      lens[3] = NCCL_UNIQUE_ID_BYTES;
-      redisReply *reply = static_cast<redisReply*>(
-        redisCommandArgv(ctx_, 3, command, lens));
-      if (reply->type == REDIS_REPLY_STATUS) {
-        ret_status = std::string(reply->str, reply->len);
-        if (ret_status == "OK") {
-          ret = true;
-        }
-      } else {
-        std::cerr << "error response type" << std::endl;
+
+    auto cmd1 = std::string("SET");
+    command.push_back(cmd1.c_str());
+    lens.push_back(cmd1.size());
+
+    command.push_back(id_key_.c_str());
+    lens.push_back(id_key_.size());
+
+    auto cmd3 = std::string(uniq_id.internal, NCCL_UNIQUE_ID_BYTES);
+    command.push_back(cmd3.c_str());
+    lens.push_back(NCCL_UNIQUE_ID_BYTES);
+
+    redisReply *reply = static_cast<redisReply*>(
+      redisCommandArgv(ctx_, command.size(), command.data(), lens.data()));
+    if (reply->type == REDIS_REPLY_STATUS) {
+      auto ret_status = std::string(reply->str, reply->len);
+      if (ret_status == "OK") {
+        ret = true;
       }
-      freeReplyObject(reply);
+    } else if (reply->type == REDIS_REPLY_ERROR) {
+      std::cerr << "error: " << reply->str << std::endl;
+    } else {
+      std::cerr << "error response type: " << reply->type << std::endl;
     }
+    freeReplyObject(reply);
+
     return ret;
   }
 
-  std::vector<ncclUniqueId> GetClusterIds(int node_count) {
-    std::vector<ncclUniqueId> ret;
-    for (int i = 0; i < node_count; ++i) {
-      while(!Exists(i)) {
-        sleep(1);
-      }
-      redisReply *reply = static_cast<redisReply*>(
-        redisCommand(ctx_, "GET trainer:%03d", i));
-      if (reply->type == REDIS_REPLY_STRING) {
-        for (int i = 0; i < reply->elements; ++i) {
-          if (per_reply->type == REDIS_REPLY_STRING) {
-            ncclUniqueId id;
-            if (reply->len != NCCL_UNIQUE_ID_BYTES) {
-              std::cerr << "redis reply uniq id data error" << std::endl;
-            }
-            memcpy(id.internal, reply->str, reply->len);
-            ret.emplace_back(id);
-          }
-        }
-      }
-      freeReplyObject(reply);
+ncclUniqueId GetClusterId(int node_count) {
+    ncclUniqueId id;
+
+    while(!Exists()) {
+      sleep(1);
     }
-    return ret;
+
+    redisReply *reply = static_cast<redisReply*>(
+      redisCommand(ctx_, "GET %s", id_key_.c_str()));
+
+    if (reply->type == REDIS_REPLY_STRING) {
+      ncclUniqueId id;
+      if (reply->len != NCCL_UNIQUE_ID_BYTES) {
+        std::cerr << "redis reply uniq id data error" << std::endl;
+        exit(1);
+      }
+      memcpy(id.internal, reply->str, reply->len);
+    }
+    freeReplyObject(reply);
+    return id;
   }
 
  private:
-  bool Exists(int trainer_id) {
-    char command[512];
+  bool Exists() {
     bool ret = false;
-    sprintf(command, "EXISTS trainer:%03d", trainer_id);
     redisReply *reply = static_cast<redisReply*>(
-        redisCommand(ctx_, command));
+        redisCommand(ctx_, "EXISTS %s", id_key_.c_str()));
     if (reply->type == REDIS_REPLY_INTEGER) {
       if (reply->integer == 1) {
         ret = true;
@@ -96,4 +99,5 @@ class NcclIdServer {
   std::string ip_;
   int port_;
   redisContext *ctx_ = nullptr;
+  const std::string id_key_ = "nccl_id_key";
 };
